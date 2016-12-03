2b7757400a79e05b0074925007acc89023089d8e
mika <mika@oss.org>
2016-12-03 13:44:52 +0800

    fix

--- rb/http_util_use_net_http.rb
+++ rb/http_util_use_net_http.rb
@@ -4,25 +4,26 @@ module HttpUtil
   def self.get(path:, query: {}, headers: {})
      uri = get_uri(path, query)
     http = get_http(uri.host, uri.port)
-     req = Net::HTTP::Get.new(uri.request_uri)
+     req = Net::HTTP::Get.new(uri.request_uri, headers)
      req = (yield req) if block_given?
     http.request(req)
   end
 
+  # body: string
   # return a Net::HTTPResponse object
-  def self.post(path:, query: {}, body:{}, headers: {})
+  def self.post(path:, query: {}, body:, headers: {})
      uri = get_uri(path, query)
     http = get_http(uri.host, uri.port)
-     req = Net::HTTP::Post.new(uri.request_uri)
-     req.set_form_data(body)
+     req = Net::HTTP::Post.new(uri.request_uri, headers)
+     req.body = body
      req = (yield req) if block_given?
     http.request(req)
   end
 
   def self.get_http(host, port)
-    http = Net::Http.new(host, port)
-    http.open_timeout = 10
-    http.read_timeout = 10
+    http = Net::HTTP.new(host, port)
+    http.open_timeout = 15
+    http.read_timeout = 15
     http
   end
 
