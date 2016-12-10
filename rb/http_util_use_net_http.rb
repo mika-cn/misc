34aa724f3c464416afb7d82080e8045cd21235e7
mika <mika@oss.org>
2016-12-10 16:17:01 +0800

    fix

--- rb/http_util_use_net_http.rb
+++ rb/http_util_use_net_http.rb
@@ -1,11 +1,13 @@
 require 'net/http'
+require 'openssl'
+
 module HttpUtil
   # return a Net::HTTPResponse object
   def self.get(path:, query: {}, headers: {})
      uri = get_uri(path, query)
-    http = get_http(uri.host, uri.port)
      req = Net::HTTP::Get.new(uri.request_uri, headers)
      req = (yield req) if block_given?
+    http = get_http(uri)
     http.request(req)
   end
 
@@ -13,23 +15,27 @@ module HttpUtil
   # return a Net::HTTPResponse object
   def self.post(path:, query: {}, body:, headers: {})
      uri = get_uri(path, query)
-    http = get_http(uri.host, uri.port)
      req = Net::HTTP::Post.new(uri.request_uri, headers)
      req.body = body
      req = (yield req) if block_given?
+    http = get_http(uri)
     http.request(req)
   end
 
-  def self.get_http(host, port)
-    http = Net::HTTP.new(host, port)
-    http.open_timeout = 15
-    http.read_timeout = 15
+  def self.get_http(uri)
+    http = Net::HTTP.new(uri.host, uri.port)
+    if 'https' == uri.scheme
+      http.verify_mode =  OpenSSL::SSL::VERIFY_NONE
+      http.use_ssl = true
+    end
+    http.open_timeout = 10
+    http.read_timeout = 10
     http
   end
 
   def self.get_uri(path, query)
     uri = URI(path)
-    uri.query = URI.encode_www_form(query)
+    uri.query = URI.encode_www_form(query) if !query.empty?
     uri
   end
 end
